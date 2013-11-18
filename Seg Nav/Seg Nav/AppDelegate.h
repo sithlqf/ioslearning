f5de6996e6c79492df486316de3f96302e7fbf19
sithlqf <sithlqf@163.com>
2013-11-17 23:14:30 -0500

    S

---
+++ Seg Nav/Seg Nav/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Seg Nav
+//
+//  Created by sithlqf on 13-11-11.
+//  Copyright (c) 2013年 sithlqf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
