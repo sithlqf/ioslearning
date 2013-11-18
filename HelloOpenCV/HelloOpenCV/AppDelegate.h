fea29535b6dcb04f191d3a439231f8ed335e5308
sithlqf <sithlqf@163.com>
2013-11-17 20:18:51 -0500

    h

---
+++ HelloOpenCV/HelloOpenCV/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloOpenCV
+//
+//  Created by sithlqf on 13-11-17.
+//  Copyright (c) 2013年 sithlqf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HelloOpenCV/HelloOpenCV/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloOpenCV
+//
+//  Created by sithlqf on 13-11-17.
+//  Copyright (c) 2013年 sithlqf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
